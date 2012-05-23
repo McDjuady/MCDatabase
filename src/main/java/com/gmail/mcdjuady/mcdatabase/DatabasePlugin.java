a66a81883133cfbc4f9b977dbd37046ca3b89e1d
Max Micko <mcdjuady@googlemail.com>
2012-05-23 16:02:19 +0200

    Made it a standalone Plugin

---
+++ src/main/java/com/gmail/mcdjuady/mcdatabase/DatabasePlugin.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gmail.mcdjuady.mcdatabase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author McDjuady
+ */
+public class DatabasePlugin extends JavaPlugin{
+    //Empty for now to use it as a central library instead of shading it into the projects
+}
